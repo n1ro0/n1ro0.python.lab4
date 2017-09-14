8d9ce84c88aa931f16f09d390dccdae813ecfa13
Ilya Chaban <n1ro0ilya@gmail.com>
2017-09-14 22:22:43 +0300

    version 2.0

--- main.py
+++ main.py
@@ -25,35 +25,36 @@ with open("file.json") as f:
 print(new_dict["surname"])"""
 
 def render():
-    res_x = 500
-    res_y = 500
-    x_min = -1.777
-    x_max = 1.777
-    y_min = -1.
-    y_max = 1.
+    res_x = 700
+    res_y = 700
+    x_min = 0
+    x_max = 0.700
+    y_min = 0.
+    y_max = 0.500
     choices = [i for i in pfs.keys()]
-    coeff = Coeff()
-    for p_count in range(10000):
-        new_x = random.randint(-1777, 1777) / 1000
-        new_y = random.randint(-1000, 1000)  / 1000
-        for step in range(-20, 4):
+    coeff = [Coeff() for i in range(10)]
+    for p_count in range(1000):
+        new_x = random.randint(1, 700) / 700
+        new_y = random.randint(1, 700)  / 500
+        for step in range(-20, 1000):
 
             choice = random.choice(choices)
+            rand = random.randint(0, 9)
+            new_x = coeff[rand].a * new_x + coeff[rand].b * new_y + coeff[rand].c;
+            new_y = coeff[rand].d * new_x + coeff[rand].e * new_y + coeff[rand].f;
+            (new_x, new_y) = pfs[choice](new_x, new_y)
 
-            new_x, new_y = pfs["sinusoidal"](new_x, new_y)
-            new_x = coeff.a * new_x + coeff.b * new_y + coeff.c;
-            new_y = coeff.d * new_x + coeff.e * new_y + coeff.f;
             if step>=0 and new_x > x_min and new_x < x_max and new_y > y_min and new_y < y_max:
                 x = res_x - ((x_max - new_x) / (x_max - x_min) * res_x);
                 y = res_y - ((y_max - new_y) / (y_max - y_min) * res_y);
                 if(x < res_x and y < res_y):
-                    image.put(str(coeff.color), to=[int(x), int(y)])
+                    image.put(str(coeff[rand].color), to=[int(x), int(y)])
 
 
 
 root = Tk()
-canvas = Canvas(root, width=500, height=500, background="black")
-image = PhotoImage(width=500, height=500)
+canvas = Canvas(root, width=700, height=500, background="black")
+image = PhotoImage(width=700, height=500)
 canvas.create_image(0, 0, image=image, anchor="nw")
 canvas.pack()
 button = Button(root, text="Draw", command=render)
